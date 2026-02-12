e64e1338625f592e972542bf902741f0ece83bcf
spector-asael <2023158061@ub.edu.bz>
2026-02-11 22:31:06 -0600

    JSON Version 3

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,27 +1,22 @@
 package main 
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
 func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
                                                r *http.Request) {
-    data := map[string]string {
+   data := map[string]string {
                                 "status": "available",
                                 "environment": a.config.environment,
                                 "version": appVersion,
                               }
-jsResponse, err := json.Marshal(data)
-	if err != nil {
-		a.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", 
-					http.StatusInternalServerError)
-		return
-	}
-    jsResponse = append(jsResponse, '\n')
-    w.Header().Set("Content-Type", "application/json")
-    w.Write(jsResponse)
-
+   err := a.writeJSON(w, http.StatusOK, data, nil)
+   if err != nil {
+    a.logger.Error(err.Error())
+    http.Error(w, "The server encountered a problem and could not process your request", 
+	http.StatusInternalServerError)
+   }
 }
+
 
