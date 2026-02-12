f606c7ccdec47b10d9deb0ff27ab15358fef3987
spector-asael <2023158061@ub.edu.bz>
2026-02-11 23:54:04 -0600

    Panic refactoring

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,12 +10,11 @@ func (a *applicationDependencies)routes() http.Handler  {
 
    // setup a new router
    router := httprouter.New()
-   // handle 404
    router.NotFound = http.HandlerFunc(a.notFoundResponse)
-  // handle 405
    router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
    // setup routes
    router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
-   return router
-}
 
+   return a.recoverPanic(router)      
+  
+}
