5b9dd5daad242c5669203d107104c8829c91f2f1
spector-asael <2023158061@ub.edu.bz>
2026-02-11 22:45:22 -0600

    JSON Version 5

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,22 +1,27 @@
 package main 
 
 import (
+
 	"net/http"
 )
 
+
 func (a *applicationDependencies)healthcheckHandler(w http.ResponseWriter,
                                                r *http.Request) {
-   data := map[string]string {
-                                "status": "available",
-                                "environment": a.config.environment,
-                                "version": appVersion,
-                              }
+   data := envelope {
+                     "status": "available",
+                     "system_info": map[string]string{
+                             "environment": a.config.environment,
+                             "version": appVersion,
+                    },
+   }
    err := a.writeJSON(w, http.StatusOK, data, nil)
    if err != nil {
     a.logger.Error(err.Error())
     http.Error(w, "The server encountered a problem and could not process your request", 
-	http.StatusInternalServerError)
+				http.StatusInternalServerError)
    }
 }
+
 
 
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,25 +5,29 @@ import (
   "net/http"
 )
 
+// create an envelope type
+type envelope map[string]any
+
 func (a *applicationDependencies)writeJSON(w http.ResponseWriter,
-                                           status int, data any,
+                                           status int, data envelope,
                                            headers http.Header) error  {
-    jsResponse, err := json.Marshal(data)
+    jsResponse, err := json.MarshalIndent(data, "", "\t")
     if err != nil {
         return err
     }
-
     jsResponse = append(jsResponse, '\n')
     // additional headers to be set
     for key, value := range headers {
         w.Header()[key] = value
-        //w.Header().Set(key, value[0])
     }
     // set content type header
     w.Header().Set("Content-Type", "application/json")
     // explicitly set the response status code
     w.WriteHeader(status) 
-    w.Write(jsResponse)
+    _, err = w.Write(jsResponse)
+    if err != nil {
+        return err
+    }
 
     return nil
 
